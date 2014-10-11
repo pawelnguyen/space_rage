9df28f7cd44cc7e8ea067fa33e9f5ed75e4a5b46
Paweł Nguyen <pawel.nguyen@gmail.com>
2014-10-11 17:53:24 +0200

    refactor asteroid

--- asteroid.rb
+++ asteroid.rb
@@ -1,45 +1,69 @@
 class Asteroid
-  attr_reader :x, :y
+  attr_reader :window, :window_height, :window_width
 
   def initialize(window, window_width, window_height)
+    @window = window
     @window_width = window_width
     @window_height = window_height
-
-    @asteroid_image = Gosu::Image.new(window, "assets/asteroid.png", true)
-    @speed = random_speed
-    @rotation_speed = random_rotation
-    @rotation = 0
-    @rotation_direction = random_rotation_direction
-    @size_scale = random_size_scale
-    @x = rand(@window_width - width / 2) + width / 2
-    @y = -height
   end
 
   def draw
-    @asteroid_image.draw_rot(@x, @y, 2, @rotation, 0.5, 0.5, @size_scale, @size_scale)
+    asteroid_image.draw_rot(x, y, 2, rotation, 0.5, 0.5, size_scale, size_scale)
   end
 
   def move
-    @y += @speed
-    @rotation += @rotation_speed * @rotation_direction
+    @y = y + speed
+    @rotation = rotation + rotation_speed * rotation_direction
   end
 
   def deletable?
-    @y - height / 2 > @window_height
+    y - height / 2 > window_height
   end
 
   def radius
     width / 2
   end
 
+  def x
+    @x ||= rand(window_width - width / 2) + width / 2
+  end
+
+  def y
+    @y ||= -height
+  end
+
   private
 
+  def asteroid_image
+    @asteroid_image ||= Gosu::Image.new(window, "assets/asteroid.png", true)
+  end
+
+  def speed
+    @speed ||= random_speed
+  end
+
+  def rotation_speed
+    @rotation_speed = random_rotation
+  end
+
+  def rotation
+    @rotation ||= 0
+  end
+
+  def rotation_direction
+    @rotation_direction ||= random_rotation_direction
+  end
+
+  def size_scale
+    @size_scale ||= random_size_scale
+  end
+
   def width
-    @asteroid_image.width * @size_scale
+    asteroid_image.width * size_scale
   end
 
   def height
-    @asteroid_image.height * @size_scale
+    asteroid_image.height * size_scale
   end
 
   def random_speed
